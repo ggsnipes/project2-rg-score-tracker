3210368f4da15b207c7f271edec37d83e3c43f26
ggsnipes <31529149+ggsnipes@users.noreply.github.com>
2023-03-14 16:11:52 +1100

    structure starting to form

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const session = require('express-session')
+
+const sessionController = require('./controllers/session_controller')
+const userController = require('./controllers/user_controller')
+
+
+const logger = require("./middleware/logger")
+
+
+app.set("view engine", "ejs")
+
+app.use(logger)
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "bbbbb",
+        resave: false,
+        saveUninitialized: true
+    })
+)
+
